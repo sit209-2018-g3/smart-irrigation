f948085106430ce2e06f8c2951d8f3fbdef318e1
Brenton Fleming <bkfl@deakin.edu.au>
2018-09-20 19:07:37 +1000

    NEW: Add stylesheet.

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Pages
 import Layout from './pages/Layout';
+
+import "./styles.css";
 
 ReactDOM.render(<Layout />, document.getElementById('root'));
